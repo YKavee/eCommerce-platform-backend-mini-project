cfdd16b15b9081adca36710e2934f5491f742553
YKavee <ykaveesenanayake@gmail.com>
2023-01-26 12:36:59 +0530

    Implemented view order detail

--- src/model/order-model/order-model.js
+++ src/model/order-model/order-model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const orderSchema = mongoose.Schema({
-  id: { type: String, required: false },
+  userId: { type: String, required: true },
   cartProducts: { type: Array, required: true },
   totalPrice: { type: Number, required: true },
 });
